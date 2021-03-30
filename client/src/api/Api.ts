7cf1ccd73b71334208a46883b85c6de9a6390dd8
Ratriel <55146317+Ratriel@users.noreply.github.com>
2021-03-30 11:14:13 -0600

    adding getEmployee method.

--- client/src/api/Api.ts
+++ client/src/api/Api.ts
@@ -1,10 +1,29 @@
 import IEmployee from "../models/IEmployee";
 import IApi from "./IApi";
 import Axios from "axios";
+import axios from "axios";
 export default class Api implements IApi {
   async getEmployees(): Promise<Array<IEmployee>> {
     const res = await Axios.get("http://localhost:8000/employee");
     return res.data;
+  }
+  async getEmployee(id: number): Promise<void> {
+    const res = await axios.get(`http://localhost:8000/employee/${id}`);
+    return res.data;
+
+    //   Axios.post("http://localhost:8000/employee", {
+    //     name: employee.name,
+    //     salary: employee.salary,
+    //   }).then(setData([...data, employee]));
+
+    //   console.log(
+    //     "Employee: ",
+    //     employee.name,
+    //     " with Salary of",
+    //     employee.salary,
+    //     ", has been added"
+    //   );
+    // };
   }
   async delEmployee(id: number): Promise<void> {}
   async addEmployee(employee: IEmployee): Promise<void> {}
--- client/src/api/IApi.ts
+++ client/src/api/IApi.ts
@@ -1,7 +1,9 @@
+import { promises } from "node:dns";
 import IEmployee from "../models/IEmployee";
 
 export default interface IApi {
   getEmployees(): Promise<Array<IEmployee>>;
-  delEmployee(id: number): Promise<void>;
+  getEmployee(id: number): Promise<void>;
   addEmployee(employee: IEmployee): Promise<void>;
+  delEmployee(id: number): Promise<void>;
 }
