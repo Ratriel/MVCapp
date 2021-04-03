db4afa98cf28abb18a29dfe1fabb82a0da3780da
Ratriel <55146317+Ratriel@users.noreply.github.com>
2021-04-02 20:24:02 -0600

    ServerTS

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+// import express from "express";
+// import cors from "cors";
+// import bodyParser from "body-parser";
+// const app: express.Application = express();
+
+// Settings
+
+// app.set("port", 8000);
+
+// Middlewares
+// app.use(cors());
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+// app.use(() => import("./routes/employees"));
+
+// Starting Server
+// app.listen(app.get("port"), () => {
+//   console.log("Server on port", app.get("port"));
+// });
+
+console.log("Init");
