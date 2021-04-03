2d68ce3d468203ec0c46f194fb030b1c76779a8a
Ratriel <55146317+Ratriel@users.noreply.github.com>
2021-04-02 19:39:19 -0600

    format

--- server/src/dataBase.js
+++ server/src/dataBase.js
@@ -1,22 +1,19 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const mysqlConnection =  mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: "company"
-})
+const mysqlConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "company",
+});
 
-mysqlConnection.connect((err)=>{
-    if(err){
-        console.log(err);
-        return;
-    }else{
-        console.log("DataBase is Connected");
-        
-    }
-})
-
+mysqlConnection.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("DataBase is Connected");
+  }
+});
 
 module.exports = mysqlConnection;
-
